2c13dc4be83d109ea3c1220af47a77d0d468e4c1
Wakhekile  Maseko <wakhekilemaseko@gmail.com>
2024-08-29 16:17:32 +0200

    update

---
+++ bakery-website/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'
+import AboutUs from './AboutUs'
+
+
+
+function Home() {
+  return (
+    <div className='App-header'>
+      
+      <div className='header'>
+        <h1 className='heading1'>Pastry Delights</h1>
+        <p className='paragraph1'>By WAKHEKILE  MASEKO </p>
+
+      </div>
+      <AboutUs/>
+
+    </div>
+  );
+}
+
+
+export default App;
